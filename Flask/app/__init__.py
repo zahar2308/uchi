379fe3ca54256f08baf8cea85c678c45dbeaea39
Zahar <zleonov151@vk.com>
2024-03-24 20:11:58 +0300

    Первая версия Проекта1

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
-# app.config['SECRET_KEY'] = 'SECRET_KEY'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# app.config.from_object('config.Config')
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
 app.app_context().push()
