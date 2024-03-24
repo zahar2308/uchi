48cdfd8fb9d74c85e7ada51c485c7248476c5f30
Zahar <zleonov151@vk.com>
2024-03-24 20:14:36 +0300

    Устранение ошибок

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# app.config.from_object('config.Config')
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
--- Flask/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv()
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
